42ddb8301c5c5f0322de1f576cc2b4d65c8416e6
Vermidia <vermidiathesnake@gmail.com>
2025-08-27 02:12:32 -0500

    syntax

--- Content.Shared/_RMC14/Projectiles/Reflect/RMCReflectSystem.cs
+++ Content.Shared/_RMC14/Projectiles/Reflect/RMCReflectSystem.cs
@@ -69,7 +69,7 @@ public sealed partial class RMCReflectSystem : EntitySystem
             {
                 fixedDist.FlyEndTime = _timing.CurTime + TimeSpan.FromSeconds(distance / gun.ProjectileSpeedModified);
                 Dirty(projectile, fixedDist);
-        }
+            }
         }
         else
         {
